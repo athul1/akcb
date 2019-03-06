907136a0ea38ef608d9254344b9d0e1560863a97
athul1 <athulkumar798@gmail.com>
2019-03-05 23:14:51 -0800

    lunch brake

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app=Flask(__name__)
 
+@app.route("/")
+def intex():
+    return render_template('index.html')
+    
+@app.route("/home")
+def home():
+    return "Welcome to Our Home Page"
+
+@app.route("/contacts")
+def contact():
+    return "These are your Contacts"
+
+@app.route("/about")
+def about():
+    return "Know more about us"
+
 if (__name__=="__main__"):
-    app.run()
+    app.run(debug=True)
